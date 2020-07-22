f9eb6628ce448352cf5070e8b4ae8d0ca403dacf
Kirill Kotelevsky <yixwellet@gmail.com>
2020-07-22 22:01:43 +0400

    список пользователей теперь принимается через fs

--- routes/cards.js
+++ routes/cards.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const cardsRouter = require('express').Router();
 
-const reader = fs.createReadStream(path.join(path.resolve(), 'data/cards.json'), { encoding: 'utf8' });
-
 cardsRouter.get('/', (req, res) => {
-  res.send(reader.pipe(res));
+  const cards = fs.createReadStream(path.join(path.resolve(), 'data/cards.json'), { encoding: 'utf8' });
+  res.set({ 'content-type': 'application/json; charset=utf-8' });
+  cards.pipe(res);
 });
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,26 @@ const fs = require('fs');
 const path = require('path');
 const usersRouter = require('express').Router();
 
-const users = fs.readFileSync(path.join(path.resolve(), 'data/users.json'), { encoding: 'utf8' });
-
+const users = fs.createReadStream(path.join(path.resolve(), 'data/users.json'));
 usersRouter.get('/', (req, res) => {
-  res.send(users);
+  res.set({ 'content-type': 'application/json; charset=utf-8' });
+  users.pipe(res);
 });
 
 usersRouter.get('/:id', (req, res) => {
-  if (!JSON.parse(users).some((user) => user._id === req.params.id)) {
+  let usersArray = '';
+  users.on('data', (chunk) => {
+    usersArray += chunk.toString();
+  });
+  users.on('end', () => {
+    console.log(JSON.parse(usersArray));
+  });
+  if (!JSON.parse(usersArray).some((user) => user._id === req.params.id)) {
     res.status(404).send({ message: 'Нет пользователя с таким id' });
     return;
   }
 
-  res.send(JSON.parse(users).find((user) => user._id === req.params.id));
+  res.send(usersArray.find((user) => user._id === req.params.id));
 });
 
 module.exports = usersRouter;
