9843fbe669ca113dd1f3e1fdcb0fa9c7bcb7c366
yixellet <yixwellet@gmail.com>
2020-07-22 08:18:57 +0400

    научил сервер раздавать статику

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const path = require('path');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
