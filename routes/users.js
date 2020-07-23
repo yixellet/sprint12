a3a36c271d7e2fb798fab26b877521b6289852ff
Kirill Kotelevsky <yixwellet@gmail.com>
2020-07-23 23:43:09 +0400

    something stupid

--- routes/users.js
+++ routes/users.js
@@ -8,20 +8,28 @@ usersRouter.get('/', (req, res) => {
   users.pipe(res);
 });
 
-usersRouter.get('/:id', (req, res) => {
-  let usersArray = '';
-  users.on('data', (chunk) => {
-    usersArray += chunk.toString();
-  });
-  users.on('end', () => {
-    console.log(JSON.parse(usersArray));
-  });
+const sendUser = (req, res, usersArray, next) => {
+  const userData = usersArray.find((user) => user._id === req.params.id);
+  res.send(userData);
+  next();
+};
+
+const doesUserExists = (req, res, usersArray, next) => {
   if (!JSON.parse(usersArray).some((user) => user._id === req.params.id)) {
     res.status(404).send({ message: 'Нет пользователя с таким id' });
     return;
   }
-
-  res.send(usersArray.find((user) => user._id === req.params.id));
+  next();
+};
+let usersArray = '';
+users.on('data', (chunk) => {
+  usersArray += chunk.toString();
 });
+users.on('end', () => {
+  console.log(JSON.parse(usersArray)[0]);
+});
+
+usersRouter.get('/:id', doesUserExists);
+usersRouter.get('/:id', sendUser);
 
 module.exports = usersRouter;
